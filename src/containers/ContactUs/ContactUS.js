93c277780cc6133a96ee1e49d8ad7634d3cf268d
pushpal99 <pushpalchakrabarty99@gmail.com>
2024-01-11 01:50:26 +0530

    added country and other

--- src/containers/ContactUs/ContactUS.js
+++ src/containers/ContactUs/ContactUS.js
@@ -1,27 +1,14 @@
 import React from "react";
-import { SITE_TEXT } from "../Home/constants";
+import {country_list} from "../Home/constants";
 import './index.scss';
 import { Button, FormControl, FormGroup, Input, MenuItem, Select } from "@mui/material";
 import Globe from '../../assets/Contact/globe.png'
 
 const ContactUs = () => {
-    // const {  
-    //     SUPERCHARGE_HEAD_TXT, 
-    //     SUPERCHARGE_SUB_HEAD,
-    //     ABOUT_PAGE_CARD_1,
-    //     ABOUT_PAGE_CARD_2,
-    //     ABOUT_HEAD_1,
-    //     ABOUT_SUB_INFO_1,
-    //     ABOUT_HEAD_2,
-    //     ABOUT_SUB_INFO_2,
-    //     ABOUT_HEAD_3,
-    //     ABOUT_PAGE_LIST,
-    //     ABOUT_SUB_INFO_3,
-    //     PHOTO_TAG,
-    // } = SITE_TEXT
+    
     return (
         <div className="contactUsWrapper">
-            <h2>Connect with us for secure solutions</h2>
+            <h2>Connect with us for secure solutions. </h2>
             <div className="formSection">
                 <div className="leftSection">
                     <FormControl fullWidth variant="outlined">
@@ -30,7 +17,7 @@ const ContactUs = () => {
                             <Input placeholder="Last name" />
                         </FormGroup>
                         <Input fullWidth placeholder="Work email"/>
-                        <Input fullWidth/>
+                        <Input fullWidth placeholder="Organisation Name"/>
                         <Input fullWidth placeholder="Phone number"/>
                         <FormControl fullWidth>
                             <Select
@@ -40,9 +27,7 @@ const ContactUs = () => {
                                 classes={{root: 'selectInput'}}
                             >
                                 <MenuItem value=""><em>Country</em></MenuItem>
-                                <MenuItem value={10}>Ten</MenuItem>
-                                <MenuItem value={20}>Twenty</MenuItem>
-                                <MenuItem value={30}>Thirty</MenuItem>
+                                {country_list.map(country => (<MenuItem value={country}>{country}</MenuItem>) )}
                             </Select>
                         </FormControl>
                         <Button>Submit</Button>
